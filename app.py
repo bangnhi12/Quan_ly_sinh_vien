3a8c355f78526570f1a17847e271d9a119f4b876
thanhanmuop <thanhanmuop@gmail.com>
2026-04-12 00:24:14 +0700

    Add user authentication and redirect logic to the main route; create .gitignore file

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from flask_login import current_user
 from extensions import db, login_manager
 from routes.auth import auth_bp
 from routes.candidate import candidate_bp
 from routes.admin import admin_bp
 from routes.student import student_bp
+from models import VaiTro
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'quanlysinhvien'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/quan_ly_sinh_vien'
@@ -22,6 +24,19 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 app.register_blueprint(candidate_bp, url_prefix='/candidate')
 app.register_blueprint(admin_bp, url_prefix='/admin')
 app.register_blueprint(student_bp, url_prefix='/student')
+
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        if current_user.vai_tro == VaiTro.ADMIN:
+            return redirect(url_for('admin.dashboard'))
+        if current_user.vai_tro == VaiTro.SINHVIEN:
+            return redirect(url_for('student.dashboard'))
+        return redirect(url_for('candidate.dashboard'))
+    return redirect(url_for('auth.login'))
+
+
 with app.app_context():
     db.create_all()
 
